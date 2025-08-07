e168ad2caf16329f09d2e019afefa16c8ed600d3
SUJAL-BANKAR <sujal.bankar2006@gmail.com>
2025-08-07 21:44:25 +0530

    Update Backend

--- agroprimeBackend/model/order.js
+++ agroprimeBackend/model/order.js
@@ -13,6 +13,16 @@ const orderSchema = new mongoose.Schema({
       quantity: Number,
     } 
   ],
+  shippingInfo: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phone: { type: String },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: String, required: true },
+    payment: { type: String, required: true }
+  },
   totalAmount: Number,
   createdAt: {
     type: Date,
